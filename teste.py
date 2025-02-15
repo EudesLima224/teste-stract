49a343321cb2bf3d57136c9dc0e657e10c432821
EudesLima224 <eudeslimanovo224@gmail.com>
2025-02-15 14:35:25 -0300

    Delete teste.py

--- teste.py
+++
@@ -1,9 +0,0 @@
-import flask
-import requests
-
-API_URL = "https://sidebar.stract.to/api"
-TOKEN = "ProcessoSeletivoStract2025"
-headers = {"Authorization": f"Bearer {TOKEN}"}
-
-response = requests.get(f'{API_URL}', headers=headers)
-print(response.text)
